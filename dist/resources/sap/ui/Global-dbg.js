48622afaf813cf174730c7f4dda5701f07ecf519
Henloh <56073307+henloh@users.noreply.github.com>
2022-09-12 16:20:02 +0200

    New Build

--- dist/resources/sap/ui/Global-dbg.js
+++ dist/resources/sap/ui/Global-dbg.js
@@ -78,7 +78,7 @@ sap.ui.define([
 		 */
 		version: "1.106.0",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20220912-1608" }
+		buildinfo : { lastchange : "", buildtime : "20220912-1616" }
 	});
 
 	var syncCallBehavior = Configuration.getSyncCallBehavior();
